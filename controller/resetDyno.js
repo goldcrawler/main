9cee719334dec675be5330201badd7f8d98a14d9
gldc <goldcrawler@outlook.com>
2021-07-12 19:39:18 +0430

    auto restart test 2

--- controller/resetDyno.js
+++ controller/resetDyno.js
@@ -1,22 +1,22 @@
 let token = '1df53a57-cdf0-47db-9287-db168e9e8716';
 let appName = 'gldc2';
+const fetch = require('node-fetch');
 
-let request = require('request');
 
 exports.restart = async ()=>{
-    request.delete(
-        {
-            url: 'https://api.heroku.com/apps/' + appName + '/dynos/',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/vnd.heroku+json; version=3',
-                'Authorization': 'Bearer ' + token
-            }
-        },
-        function(error, response, body) {
+    fetch('https://api.heroku.com/apps/' + appName + '/dynos/', {
+        method: 'delete',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/vnd.heroku+json; version=3',
+            'Authorization': 'Bearer ' + token
+        }    })
+        .then(res => res.json())
+        .then(json => {
+            console.log("server restarted");
+            console.log(json);
+        })
+        .catch(error => {
             console.log(error);
-            console.log("restarted");
-        }
-    );
-
+        });
 }

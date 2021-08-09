77ee5a6fc03456079899bdc92e030b3ebb6b9bd5
gldc <goldcrawler@outlook.com>
2021-08-09 11:58:20 +0430

    chage server ip address

--- controller/prices.js
+++ controller/prices.js
@@ -7,7 +7,7 @@ exports.kimia = async (req, res, next) => {
     const adapter = new FileSync('db.json')
     const db = low(adapter)
     let ip = req.headers['x-forwarded-for']
-    if (!ip.startsWith("188.0.241.9")){
+    if (!ip.startsWith("91.98.100.195")){
         console.log(ip)
         res.status(404).send("page not found")
     }else {
